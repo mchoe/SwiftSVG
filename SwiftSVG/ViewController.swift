6a3c53c3031effbaff908fe300aa963b947c427e
mchoe <michael@straussmade.com>
2015-03-07 06:17:30 -0500

    Initial Commit

---
+++ SwiftSVG/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSVG
+//
+//  Created by collardgreens on 3/7/15.
+//  Copyright (c) 2015 Strauss LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
