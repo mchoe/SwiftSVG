970b2625f6ce91e5619f1d7162c18119333e0239
Michael Choe <michael@straussmade.com>
2018-02-19 20:39:36 -0500

    NSDataAsset #available

--- SwiftSVG/SVG Extensions/SVGView.swift
+++ SwiftSVG/SVG Extensions/SVGView.swift
@@ -71,10 +71,12 @@ open class SVGView : UIView {
                 CALayer(SVGURL: url) { [weak self] (svgLayer) in
                     self?.nonOptionalLayer.addSublayer(svgLayer)
                 }
-            } else if #available(iOS 10.11, *), let asset = NSDataAsset(name: thisName) {
-                let data = asset.data
-                CALayer(SVGData: data) { [weak self] (svgLayer) in
-                    self?.nonOptionalLayer.addSublayer(svgLayer)
+            } else if #available(iOS 10.11, *) {
+                if let asset = NSDataAsset(name: thisName) {
+                    let data = asset.data
+                    CALayer(SVGData: data) { [weak self] (svgLayer) in
+                        self?.nonOptionalLayer.addSublayer(svgLayer)
+                    }
                 }
             }
         }
