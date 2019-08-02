66504beef4c5771734bcbcfad2178797815ee779
Michael Choe <michael@straussmade.com>
2019-08-01 17:01:29 -1000

    Added tests

--- SwiftSVGTests/UIColorExtensionsTests.swift
+++ SwiftSVGTests/UIColorExtensionsTests.swift
@@ -64,6 +64,41 @@ class UIColorExtensionsTests: XCTestCase {
         XCTAssertTrue(colorArray[2] == 0, "Expected 0, got \(colorArray[2])")
     }
     
+    func testHexStringWithAlpha() {
+        var testString = "#fcab1def"
+        var testColor = UIColor(hexString: testString)
+        var colorArray = self.colorArray(testColor!)
+        XCTAssertTrue(colorArray[0] == 252 / 255, "Expected \(252 / 255), got \(colorArray[0])")
+        XCTAssertTrue(colorArray[1] == 171 / 255, "Expected \(171 / 255), got \(colorArray[1])")
+        XCTAssertTrue(colorArray[2] == 29 / 255, "Expected \(29 / 255), got \(colorArray[2])")
+        XCTAssertTrue(colorArray[3] == 239 / 255, "Expected \(239 / 255), got \(colorArray[2])")
+        
+        testString = "a6bfc4d0"
+        testColor = UIColor(hexString: testString)
+        colorArray = self.colorArray(testColor!)
+        XCTAssertTrue(colorArray[0] == 166 / 255, "Expected \(166 / 255), got \(colorArray[0])")
+        XCTAssertTrue(colorArray[1] == 191 / 255, "Expected \(191 / 255), got \(colorArray[1])")
+        XCTAssertTrue(colorArray[2] == 196 / 255, "Expected \(196 / 255), got \(colorArray[2])")
+        XCTAssertTrue(colorArray[3] == 208 / 255, "Expected \(208 / 255), got \(colorArray[2])")
+    }
+    
+    func testShortHexStrings() {
+        var testString = "#30f"
+        var testColor = UIColor(hexString: testString)
+        var colorArray = self.colorArray(testColor!)
+        XCTAssertTrue(colorArray[0] == 0.2, "Expected 0.2, got \(colorArray[0])")
+        XCTAssertTrue(colorArray[1] == 0, "Expected 0, got \(colorArray[1])")
+        XCTAssertTrue(colorArray[2] == 1.0, "Expected 1.0, got \(colorArray[2])")
+        
+        testString = "f033"
+        testColor = UIColor(hexString: testString)
+        colorArray = self.colorArray(testColor!)
+        XCTAssertTrue(colorArray[0] == 1, "Expected 1, got \(colorArray[0])")
+        XCTAssertTrue(colorArray[1] == 0, "Expected 0, got \(colorArray[1])")
+        XCTAssertTrue(colorArray[2] == 0.2, "Expected 0.2, got \(colorArray[2])")
+        XCTAssertTrue(colorArray[3] == 0.2, "Expected 0.2, got \(colorArray[3])")
+    }
+    
     func testRGBString() {
         let testString = "rgb(255, 255, 0)"
         let testColor = UIColor(rgbString: testString)
